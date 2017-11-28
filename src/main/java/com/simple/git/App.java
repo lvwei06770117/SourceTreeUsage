05b29b74bc9709aba3f4171abf74db113447d058
WENXUE\lvwei01 <lvwei19881004@gmail.com>
2017-11-28 14:01:45 +0800

    update

--- src/main/java/com/simple/git/App.java
+++ src/main/java/com/simple/git/App.java
@@ -9,5 +9,13 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        System.out.println(   ipToLong("116.228.88.18"));
+    }
+
+
+    public static Long ipToLong(String strIp) {
+        String[] ip = strIp.split("\\.");
+        return (Long.parseLong(ip[0]) << 24) + (Long.parseLong(ip[1]) << 16) + (Long.parseLong(ip[2]) << 8)
+                + Long.parseLong(ip[3]);
     }
 }
